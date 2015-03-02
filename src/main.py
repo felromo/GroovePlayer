06fe365a598984281969a6255bd3925d4315110a
Tyrantz <raymanv3@gmail.com>
2015-03-02 17:04:36 +0000

    adds the pygrooveshark module manually since the pip link is outdated, also adds main.py functionality where program plays music and takes songs from a fifo file

---
+++ src/main.py
@@ -0,0 +1,49 @@
+import subprocess
+import os
+import sys
+import threading
+# from __future__ import print_function
+# import __future__
+from grooveshark import Client
+
+client = Client()
+client.init()
+
+
+class song_listener(threading.Thread):
+    def __init__(self, playlist, fifo_path):
+        threading.Thread.__init__(self)
+        self.fifo_path = fifo_path
+        self.daemon = True
+
+    def run(self):
+        print "waiting for song"
+        fifo = open(self.fifo_path, 'r')
+        for line in fifo:
+            playlist.append(line)
+            print line + " appended"
+        fifo.close()
+
+
+class music_player(threading.Thread):
+    def __init__(self, playlist):
+        threading.Thread.__init__(self)
+        self.playlist = playlist
+
+    def run(self):
+        for play in self.playlist:
+            for song in client.search(play, type=Client.SONGS):
+                print(song)
+                subprocess.call(['cvlc', "--play-and-exit", song.stream.url])
+                playlist.pop(0)
+                break
+
+
+if __name__ == '__main__':
+    playlist = []
+    path = '/tmp/fifo'
+    listener = song_listener(playlist, path)
+    player = music_player(playlist)
+
+    listener.start()
+    player.start()
